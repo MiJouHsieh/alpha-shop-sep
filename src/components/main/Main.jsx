c7fbc1ddd751677f33020536b77ac491fbddd41a
MiJouHsieh <mijouhsieh@gmail.com>
2023-10-01 11:15:46 +0800

    style Main區塊: className 改為 camelCase

--- src/components/main/Main.jsx
+++ src/components/main/Main.jsx
@@ -1,12 +1,17 @@
 // import Register from "./components/main/register/Register";
 import Register from "./register/Register";
+import ProgressControl from './progressControl/ProgressControl'
+import styles from './Main.module.scss'
+import Cart from './cart/Cart'
 
 export default function Main() {
   return (
-    <main className="site-main">
-      <div className="main-container">
+    <main className={styles.siteMain}>
+      <div className={styles.mainContainer}>
         <Register />
+        <Cart />
+        <ProgressControl />
       </div>
     </main>
-  )
+  );
 }
