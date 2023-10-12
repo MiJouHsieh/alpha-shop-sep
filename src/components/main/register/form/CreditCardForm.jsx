9dd18240e41e5fc86e9c7faf9dd49e0215fdcaaf
MiJouHsieh <mijouhsieh@gmail.com>
2023-10-13 02:28:43 +0800

    refactor CreditCardForm: 元件化

--- src/components/main/register/form/CreditCardForm.jsx
+++ src/components/main/register/form/CreditCardForm.jsx
@@ -20,28 +20,12 @@ function InputGroup3({ inputLabel, placeholder }) {
 export default function CreditCardForm() {
   return (
     <>
-      {/* <InputGroup4 inputLabel="持卡人姓名" placeholder="John Doe" /> */}
-      <div className="col col-12">
-        <div className={`${styles.inputGroup} input-w-lg-4 input-w-sm-full`}>
-          <div className="input-label">持卡人姓名</div>
-          <input type="text" placeholder="John Doe" />
-        </div>
-      </div>
-      <div className="col col-12">
-        <div className={`${styles.inputGroup}  input-w-lg-4 input-w-sm-full`}>
-          <div className="input-label">卡號</div>
-          <input type="text" placeholder="1111 2222 3333 4444" />
-        </div>
-      </div>
-      <div className="col col-12">
-        <div className={`${styles.inputGroup} input-w-lg-3 input-w-sm-s3`}>
-          <div className="input-label">有效期限</div>
-          <input type="text" placeholder="MM/YY" />
-        </div>
-        <div className={`${styles.inputGroup} input-w-lg-3 input-w-sm-s3`}>
-          <div className="input-label">CVC / CCV</div>
-          <input type="text" placeholder="123" />
-        </div>
+      <InputGroup4 inputLabel="持卡人姓名" placeholder="John Doe" />
+      <InputGroup4 inputLabel="卡號" placeholder="1111 2222 3333 4444" />
+
+      <div className={`${styles.row} col col-12`}>
+        <InputGroup3 inputLabel="有效期限" placeholder="MM/YY" />
+        <InputGroup3 inputLabel="CVC / CCV" placeholder="123" />
       </div>
     </>
   );
