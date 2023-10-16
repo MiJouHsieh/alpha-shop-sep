ab3d3f374e096d84d23557ddc61c878d86dbc2c2
MiJouHsieh <mijouhsieh@gmail.com>
2023-10-16 15:33:11 +0800

    style Form: styles 移至 Register.module.scss

--- src/components/main/register/form/Form.jsx
+++ src/components/main/register/form/Form.jsx
@@ -1,4 +1,5 @@
-import styles from "./Form.module.scss";
+// import styles from "./Form.module.scss";
+import styles from "components/main/register/Register.module.scss";
 
 export default function Form({ children, dataPhase, formTitle }) {
   return (
