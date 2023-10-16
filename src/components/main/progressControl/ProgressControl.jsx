dff2b30c33712323e05771f8edacb998db076eb0
MiJouHsieh <mijouhsieh@gmail.com>
2023-10-16 15:38:57 +0800

    style ProgressControl: styles 移至 Register.module.scss

--- src/components/main/progressControl/ProgressControl.jsx
+++ src/components/main/progressControl/ProgressControl.jsx
@@ -1,61 +1,67 @@
 // import rightArrow from "../../../assets/icons/right-arrow.svg";
 import rightArrow from 'assets/icons/right-arrow.svg'
+import leftArrow from "assets/icons/left-arrow.svg";
+// import styles from "./ProgressControl.module.scss";
+import styles from "components/main/register/Register.module.scss";
 
-import styles from "./ProgressControl.module.scss";
-export default function ProgressControl() {
+function ButtonGroup({dataPhase,children}) {
+  return (
+    <section
+      className={`${styles.buttonGroup} col col-12`}
+      data-phase={dataPhase}
+      key={dataPhase}
+    >
+      {children}
+    </section>
+  );
+}
+
+function PrevButton({ onClick }) {
+  return (
+    <button className={styles.prev} onClick={onClick}>
+      <object
+        data={leftArrow}
+        className={styles.cursorPoint}
+        aria-label="left-arrow"
+      ></object>
+      上一步
+    </button>
+  );
+}
+
+function NextButton({ onClick }) {
+  return (
+    <button className={styles.next} onClick={onClick}>
+      下一步
+      <object
+        data={rightArrow}
+        className={styles.cursorPoint}
+        aria-label="right-arrow"
+      ></object>
+    </button>
+  );
+}
+export default function ProgressControl({onClick}) {
   return (
     <section
       className={`${styles.progressControlContainer} col col-lg-6 col-sm-12`}
     >
-      <section
-        className={`${styles.buttonGroup} col col-12`}
-        data-phase="address"
-      >
-        <button className={`${styles.next} ${styles.cursorPoint}`}>
-          下一步
-          <object
-            data={rightArrow}
-            className={styles.cursorPoint}
-            aria-label="right-arrow"
-          ></object>
-        </button>
-      </section>
+      <ButtonGroup dataPhase="address" key="address">
+      {/* <ButtonGroup dataPhase="1" key="1"> 透過SCSS條件呈現 不是帶入數字 */}
+        <NextButton onClick={onClick} />
+      </ButtonGroup>
 
-      {/* <section
-        className={`${styles.buttonGroup} col col-12`}
-        data-phase="shipping"
-      >
-        <button className={styles.prev}>
-          <object
-            data={leftArrow}
-            className={styles.cursorPoint}
-            aria-label="left-arrow"
-          ></object>
-          上一步
+      <ButtonGroup dataPhase="shipping" key="shipping">
+        <PrevButton onClick={onClick} />
+        <NextButton onClick={onClick} />
+      </ButtonGroup>
+
+      <ButtonGroup dataPhase="credit-card" key="credit-card">
+        <PrevButton onClick={onClick} />
+        <button className={styles.next} onClick={onClick}>
+          確認下單
         </button>
-        <button className={styles.next}>
-          下一步
-          <object
-            data={rightArrow}
-            className={styles.cursorPoint}
-            aria-label="right-arrow"
-          ></object>
-        </button>
-      </section>
-      <section
-        className={`${styles.buttonGroup} col col-12`}
-        data-phase="credit-card"
-      >
-        <button className={styles.prev}>
-          <object
-            data={leftArrow}
-            className={styles.cursorPoint}
-            aria-label="left-arrow"
-          ></object>
-          上一步
-        </button>
-        <button className={styles.next}>確認下單</button>
-      </section> */}
+      </ButtonGroup>
     </section>
   );
 }
